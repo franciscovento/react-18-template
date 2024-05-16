ec56f70119b7defb882017fbe1fc57a168f96db2
venfran <fgvr92@gmail.com>
2024-05-16 16:59:28 -0300

    feat: adding vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    include: ['src/**/*.test.tsx'],
+    setupFiles: './tests/setup.ts',
+    coverage: {
+      include: ['src/ui/**/*.{ts,tsx}'],
+      exclude: [
+        ...configDefaults.exclude, // toda la config para excluir modules
+        'src/ui/**/**/*.stories.{ts,tsx}',
+      ],
+    },
+  }
 })
